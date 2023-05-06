43d8d5bd235a0a3c5e6714ade5ed5ac5cb7370a0
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-05-06 00:33:38 +0000

    footer centerd, finally

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -4,6 +4,10 @@ import React from "react";
 
 export const Footer = () => {
     return(
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark"></nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+             <div class="container-fluid">
+                <a class="navbar-brand mx-auto" href="#">© 2020 Copyright:</a>
+            </div>
+        </nav>
     )
     }
