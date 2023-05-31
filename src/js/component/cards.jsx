c38f55c94aa92f5f61343117dedb74c6670508ca
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-05-31 20:59:01 +0000

    cards render different text now

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,74 +1,48 @@
 import React from "react";
 import './index.css';
 
-
 export const Cards = () => {
+  const cardData = [
+    {
+      title: "Card 1",
+      text: "This is the content for card 1.",
+      buttonLabel: "Button 1"
+    },
+    {
+      title: "Card 2",
+      text: "This is the content for card 2.",
+      buttonLabel: "Button 2"
+    },
+    {
+      title: "Card 3",
+      text: "This is the content for card 3.",
+      buttonLabel: "Button 3"
+    },
+    {
+      title: "Card 4",
+      text: "This is the content for card 4.",
+      buttonLabel: "Button 4"
+    },
+  ];
+
   return (
     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 cards-container">
-      <div className="col mb-4">
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-            Praesent rutrum nulla elit, vitae vulputate est dapibus sed. Nunc pharetra, nibh eget accumsan feugiat, magna nisi sagittis erat, non ultrices ligula arcu ut risus. Cras tincidunt dolor diam, id bibendum neque faucibus at. 
-            </p>
-            <div className = "btn-wrapper card-footer text-muted">
+      {cardData.map((card, index) => (
+        <div className="col mb-4" key={index}>
+          <div className="card">
+            <img className="card-img-top" src="" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">{card.title}</h5>
+              <p className="card-text">{card.text}</p>
+              <div className="btn-wrapper card-footer text-muted">
                 <a href="#" className="card-btn btn btn-primary">
-                    Find out More!
+                  {card.buttonLabel}
                 </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="col mb-4">
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed sodales dui, vitae molestie diam. Vivamus gravida purus vel lorem efficitur auctor. Sed pretium mi sed erat rhoncus, nec tincidunt velit ullamcorper.
-            </p>
-            <div className = "btn-wrapper card-footer text-muted">
-                <a href="#" className="card-btn btn btn-primary">
-                Find out More!
-                </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col mb-4">
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed sodales dui, vitae molestie diam. Vivamus gravida purus vel lorem efficitur auctor. Sed pretium mi sed erat rhoncus, nec tincidunt velit ullamcorper.
-            </p>
-            <div className = "btn-wrapper card-footer text-muted">
-                <a href="#" className="card-btn btn btn-primary">
-                Find out More!
-                </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="col mb-4">
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed sodales dui, vitae molestie diam. Vivamus gravida purus vel lorem efficitur auctor. Sed pretium mi sed erat rhoncus, nec tincidunt velit ullamcorper.
-            </p>
-            <div className="btn-wrapper card-footer text-muted">
-                <a href="#" className="card-btn btn btn-primary">
-                  Find out More!
-                </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
